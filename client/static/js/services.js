f3a0ab3728b6686fd9f83b2276b30bcf583ac6e4
John Herrlin <nils@vwifi-student-96.lnu.se>
2015-09-03 14:14:19 +0200

    API server changed from local to production

--- client/static/js/services.js
+++ client/static/js/services.js
@@ -1,7 +1,7 @@
 angular.module('kodkollektivet.services', [])
 
     .factory('Info', function($resource) {
-        return $resource('http://127.0.0.1:8000/info/');
+        return $resource('http://api.kodkollektivet.se/info/');
     })
 
 
