e8ea58a7085aa8c714a4c3989ea4831253a13cc1
rasmus <ras.sjostrom@hotmail.com>
2015-09-06 01:54:52 +0200

    removed a bunch of unnecessary bower installation files and added a github extension for the markdown

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -25,6 +25,13 @@ angular.module('kodkollektivet', [
         });
     })
 
+    .config(['markdownConverterProvider', function (markdownConverterProvider) {
+        // options to be passed to Showdown
+        // see: https://github.com/coreyti/showdown#extensions
+        markdownConverterProvider.config({
+            extensions: ['github']
+        });
+    }])
 
     .config(
     function ($locationProvider, $httpProvider, $resourceProvider, $stateProvider, $stickyStateProvider, $urlRouterProvider, uiGmapGoogleMapApiProvider){
