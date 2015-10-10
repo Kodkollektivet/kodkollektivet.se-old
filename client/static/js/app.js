62be7630874aba0650eaf325af09ceda49d8d67e
Robin Karlsson <rk222ev@student.lnu.se>
2015-10-10 18:48:07 +0200

    Removed things and reimplemented them

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -1,45 +1,6 @@
 angular.module('kodkollektivet', [
-    'ngRoute',
     'ngResource',
-    'ngSanitize',    
     'ui.bootstrap',
     'kodkollektivet.controllers',
     'kodkollektivet.factories',
-])
-
-    .config(function ($locationProvider, $httpProvider, $resourceProvider, $routeProvider){
-
-        // This only works in angular 3!
-        // It makes dealing with Django slashes at the end of everything easier.
-        $resourceProvider.defaults.stripTrailingSlashes = false;
-
-        // Django expects jQuery like headers
-        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-
-
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: true
-        });
-
-	$routeProvider.
-	    when('/', {
-		templateUrl: 'templates/projects.html',
-		controller: 'ProjectsCtrl'
-	    })
-	    // when('/project/:slug', {
-	    // 	templateUrl:
-	    // 	controller:
-	    // }).
-	    // when('/members', {
-	    // 	templateUrl:
-	    // 	controller:
-	    // }).
-	    // when('/member/:slug', {
-	    // 	templateUrl:
-	    // 	controller:
-	    // })
-	    
-
-	.otherwise({redirectTo: '/'});
-    });
+]);
--- client/static/js/controllers.js
+++ client/static/js/controllers.js
@@ -1,47 +1,12 @@
 angular.module('kodkollektivet.controllers', [])
 
-    .controller('ProjectsCtrl', function($scope, $routeParams, Project){
-	Project.query(function(response){
-	    $scope.projects = response.results;
-	});
-    })
+    .controller('ProjectsCtrl', function($scope, Project, ProCon){
 
-    .controller('DetailProjectController', function ($scope, $state, $stateParams, Project, SharedData) {
-
-        var selectedProject = SharedData.getProject();
-        $scope.project = selectedProject;
-        $scope.projectSlug = selectedProject.slug;
-    })
-
-    .controller('ContributorController', function ($scope, $http, Contributor, SharedData) {
-
-        Contributor.query(function(response){
-            $scope.contributors = response;
+        Project.query(function(response){
+            $scope.projects = response.results;
         });
 
-        $scope.slide = function() {
-            $.fn.fullpage.moveSlideRight();
-        };
-    })
-
-    .controller('ContactController', function($scope, Contact){
-
-        $scope.contact = {};
-        $scope.contact.name = '';
-        $scope.contact.email = '';
-        $scope.contact.tel = '';
-        $scope.contact.text = '';
-
-        $scope.submitContactForm = function(){
-            Contact.save($scope.contact,
-                function(data){
-                    //success callback
-                    alert('Thank you!\nWe contact you soon!\nBest regards\n\t\t / Kodkollektivet');
-
-                },
-                function(err){
-                    // error callback
-                }
-            );
-        };
+        ProCon.query(function(response){
+            $scope.procon = response;
+        });
     });
